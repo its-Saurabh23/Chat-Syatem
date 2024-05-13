a8080847ceee270c6f285fd6917836198e8228f3
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-05-13 13:47:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const io = require('socket.io')(http)
 io.on('connection', (socket) => {
     console.log('Connected...')
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
+        socket.broadcast.emit('message',msg)
     })
 
 })
