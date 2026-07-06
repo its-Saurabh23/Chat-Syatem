50cb5bb5f3dc09772197b61794ba653acd4e4ad5
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-07-06 23:23:10 +0530

    Update server.js remove unwanted space

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-//Socket 
-const io = require('socket.io')(http)
 
+const io = require('socket.io')(http)
 io.on('connection', (socket) => {
     console.log('Connected...')
     socket.on('message', (msg) => {
